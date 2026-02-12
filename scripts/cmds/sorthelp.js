c28f13b532e06b6332de542154981b8085474704
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2026-02-12 14:33:47 +0100

    Update sorthelp.js

--- scripts/cmds/sorthelp.js
+++ scripts/cmds/sorthelp.js
@@ -1,17 +1,17 @@
 module.exports = {
 	config: {
-		name: "sorthelp",
+		name: "فرز",
 		version: "1.2",
 		author: "NTKhang",
 		countDown: 5,
 		role: 0,
 		description: {
 			vi: "Sắp xếp danh sách help",
-			en: "Sort help list"
+			en: "فرز قائمة الأوامر"
 		},
-		category: "image",
+		category: "خدمات",
 		guide: {
-			en: "{pn} [name | category]"
+			en: "{pn} [الأسماء | الفئات]"
 		}
 	},
 
@@ -21,17 +21,17 @@ module.exports = {
 			savedCategory: "Đã lưu cài đặt sắp xếp danh sách help theo thứ tự thể loại"
 		},
 		en: {
-			savedName: "Saved sort help list by name",
-			savedCategory: "Saved sort help list by category"
+			savedName: " ✅ | تم فرز قائمة الأوامر بواسطة الأسماء",
+			savedCategory: " ✅ | تم حفظ قائمة الأوامر بواسطة الفئات"
 		}
 	},
 
 	onStart: async function ({ message, event, args, threadsData, getLang }) {
-		if (args[0] == "name") {
+		if (args[0] == "الأسماء") {
 			await threadsData.set(event.threadID, "name", "settings.sortHelp");
 			message.reply(getLang("savedName"));
 		}
-		else if (args[0] == "category") {
+		else if (args[0] == "الفئات") {
 			threadsData.set(event.threadID, "category", "settings.sortHelp");
 			message.reply(getLang("savedCategory"));
 		}
