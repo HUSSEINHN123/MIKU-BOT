5470fab62a39c4514920d679157fec5338c456cb
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2026-02-12 06:08:49 +0100

    Update language support and descriptions in antichangeinfobox

--- scripts/cmds/antichangeinfobox.js
+++ scripts/cmds/antichangeinfobox.js
@@ -2,27 +2,31 @@ const { getStreamFromURL, uploadImgbb } = global.utils;
 
 module.exports = {
 	config: {
-		name: "antichangeinfobox",
-		version: "1.9",
+		name: "حماية",
+		version: "1.8",
 		author: "NTKhang",
 		countDown: 5,
 		role: 0,
-		description: {
-			vi: "Bật tắt chức năng chống thành viên đổi thông tin box chat của bạn",
-			en: "Turn on/off anti change info box"
+		shortDescription: {
+			vi: "Chống đổi thông tin box chat",
+			en: "مكافح تغيير معلومات المجموعة"
 		},
-		category: "box chat",
+		longDescription: {
+			vi: "Bật tắt chức năng chống thành viên đổi thông tin box chat của bạn",
+			en: "قم بتشغيل أة إيقاف مكافحة_تغيير_معلومات_المجموعة"
+		},
+		category: "المجموعة",
 		guide: {
 			vi: "   {pn} avt [on | off]: chống đổi avatar box chat"
 				+ "\n   {pn} name [on | off]: chống đổi tên box chat"
 				+ "\n   {pn} nickname [on | off]: chống đổi nickname trong box chat"
 				+ "\n   {pn} theme [on | off]: chống đổi theme (chủ đề) box chat"
 				+ "\n   {pn} emoji [on | off]: chống đổi trạng emoji box chat",
-			en: "   {pn} avt [on | off]: anti change avatar box chat"
-				+ "\n   {pn} name [on | off]: anti change name box chat"
-				+ "\n   {pn} nickname [on | off]: anti change nickname in box chat"
-				+ "\n   {pn} theme [on | off]: anti change theme (chủ đề) box chat"
-				+ "\n   {pn} emoji [on | off]: anti change emoji box chat"
+			en: "   {pn} صورة [تشغيل | إيقاف]: مكافحة تغيير صورة المجموعة"
+				+ "\n   {pn} الإسم [تشغيل | إيقاف]: مكافحة تغيير إسم المجموعة"
+				+ "\n   {pn} اللقب [تشغيل | إيقاف]: مكافحة تغيير لقب الأعضاء"
+				+ "\n   {pn} السمة [تشغيل | إيقاف]: مكافحة تغيير السمة (theme) للمجموعة"
+				+ "\n   {pn} إيموجي [تشغيل | إيقاف]: مكافحة تغيير إيموجي المجموعة"
 		}
 	},
 
@@ -47,23 +51,23 @@ module.exports = {
 			antiChangeEmojiAlreadyOn: "Hiện tại box chat của bạn đang bật chức năng cấm thành viên đổi emoji"
 		},
 		en: {
-			antiChangeAvatarOn: "Turn on anti change avatar box chat",
-			antiChangeAvatarOff: "Turn off anti change avatar box chat",
-			missingAvt: "You have not set avatar for box chat",
-			antiChangeNameOn: "Turn on anti change name box chat",
-			antiChangeNameOff: "Turn off anti change name box chat",
-			antiChangeNicknameOn: "Turn on anti change nickname box chat",
-			antiChangeNicknameOff: "Turn off anti change nickname box chat",
-			antiChangeThemeOn: "Turn on anti change theme box chat",
-			antiChangeThemeOff: "Turn off anti change theme box chat",
-			antiChangeEmojiOn: "Turn on anti change emoji box chat",
-			antiChangeEmojiOff: "Turn off anti change emoji box chat",
-			antiChangeAvatarAlreadyOn: "Your box chat is currently on anti change avatar",
-			antiChangeAvatarAlreadyOnButMissingAvt: "Your box chat is currently on anti change avatar but your box chat has not set avatar",
-			antiChangeNameAlreadyOn: "Your box chat is currently on anti change name",
-			antiChangeNicknameAlreadyOn: "Your box chat is currently on anti change nickname",
-			antiChangeThemeAlreadyOn: "Your box chat is currently on anti change theme",
-			antiChangeEmojiAlreadyOn: "Your box chat is currently on anti change emoji"
+			antiChangeAvatarOn: "✅تم تشغيل مضاد تغيير صورة المجموعة",
+			antiChangeAvatarOff: "❌تم إيقاف مضاد تغيير صورة المجموعة",
+			missingAvt: "أنت لم تقم بضبط أڤتار للمجموعة أصلا",
+			antiChangeNameOn: "✅تم تشغيل مضاد تغيير إسم المجموعة",
+			antiChangeNameOff: "❌تم إيقاف مضاد تغيير إسم المجموعة",
+			antiChangeNicknameOn: "✅تم تشغيل مضاد تغيير اللقب بالنسبة للأعضاء",
+			antiChangeNicknameOff: "❌تم إيقاف تشغيل مضاد تغيير اللقب بالنسبة للأعضاء",
+			antiChangeThemeOn: "✅تم تشغيل مضاد تغيير السمة بالنسبة للمجموعة",
+			antiChangeThemeOff: "❌تم إيقاف مضاد تغيير السمة بالنسبة للمجموعة",
+			antiChangeEmojiOn: "✅تم تشغيل مضاد تغيير إيموجي المجموعة",
+			antiChangeEmojiOff: "❌تم إيقاف تشغيل مضاد تغيير إيموجي المجموعة",
+			antiChangeAvatarAlreadyOn: "مجموعتك حاليا في وضع مضاد تغيير صورة المجموعة",
+			antiChangeAvatarAlreadyOnButMissingAvt: "مجموعتك حاليا في وضع مضاد تغيير صورة المجموعة لكن على مايبدو أنه لم يتم ضبط صورة للمجموعة",
+			antiChangeNameAlreadyOn: "مجموعتك حاليا في وضع مضاد تغيير إسم المجموعة",
+			antiChangeNicknameAlreadyOn: "مجموعتك حاليا في وضع مضاد تغيير اللقب بالنسة للأعضاء",
+			antiChangeThemeAlreadyOn: "مجموعتك حاليا في وضع مضاد تغيير السمة",
+			antiChangeEmojiAlreadyOn: "مجموعتك حاليا في وضع مضاد تغيير إيموجي المجموعة"
 		}
 	},
 
@@ -83,8 +87,8 @@ module.exports = {
 			message.reply(getLang(`antiChange${key.slice(0, 1).toUpperCase()}${key.slice(1)}${args[1].slice(0, 1).toUpperCase()}${args[1].slice(1)}`));
 		}
 		switch (args[0]) {
-			case "avt":
-			case "avatar":
+			case "الصورة":
+			case "صورة_رمزية":
 			case "image": {
 				const { imageSrc } = await threadsData.get(threadID);
 				if (!imageSrc)
@@ -93,22 +97,22 @@ module.exports = {
 				await checkAndSaveData("avatar", newImageSrc.image.url);
 				break;
 			}
-			case "name": {
+			case "الإسم": {
 				const { threadName } = await threadsData.get(threadID);
 				await checkAndSaveData("name", threadName);
 				break;
 			}
-			case "nickname": {
+			case "اللقب": {
 				const { members } = await threadsData.get(threadID);
 				await checkAndSaveData("nickname", members.map(user => ({ [user.userID]: user.nickname })).reduce((a, b) => ({ ...a, ...b }), {}));
 				break;
 			}
-			case "theme": {
+			case "السمة": {
 				const { threadThemeID } = await threadsData.get(threadID);
 				await checkAndSaveData("theme", threadThemeID);
 				break;
 			}
-			case "emoji": {
+			case "الإيموجي": {
 				const { emoji } = await threadsData.get(threadID);
 				await checkAndSaveData("emoji", emoji);
 				break;
