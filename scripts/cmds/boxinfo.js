36b17982ce211bd676c27c824ee288a03b4a7a2e
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2026-02-12 16:14:38 +0100

    Update boxinfo.js

--- scripts/cmds/boxinfo.js
+++ scripts/cmds/boxinfo.js
@@ -3,17 +3,17 @@ const request = require("request");
 
 module.exports = {
 config: {
-    name: "groupinfo",
-    aliases: ['boxinfo'],
+    name: "مجموعتي",
+    aliases: ['جروبي'],
     version: "1.0",
     author: "xemon",
     countDown: 5,
     role: 0,
-    shortDescription: "See Box info",
+    shortDescription: "معلومات المجموعة",
     longDescription: "",
-    category: "box chat",
+    category: "معلومات",
     guide: {
-      en: "{p} [groupinfo|boxinfo]",
+      en: "{p} [مجموعتي|جروبي]",
     }
   },
 
@@ -52,7 +52,7 @@ const name = infu[qtv2[i].id].name;
       var callback = () =>
         api.sendMessage(
           {
-            body: `🔧「 𝐆𝐂 𝐍𝐚𝐦𝐞 」:${threadName}\n🔧「 𝐆𝐫𝐨𝐮𝐩 𝐈𝐃 」: ${id}\n🔧「 𝐀𝐩𝐩𝐫𝐨𝐯𝐚𝐥 」: ${pd}\n🔧「 𝐄𝐦𝐨𝐣𝐢 」: ${icon}\n🔧「 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 」: 𝐈𝐧𝐜𝐥𝐮𝐝𝐢𝐧𝐠 ${threadMem} 𝐌𝐞𝐦𝐛𝐞𝐫𝐬\n🔧「 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐌𝐚𝐥𝐞𝐬 」: ${nam}\n🔧「 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐅𝐞𝐦𝐚𝐥𝐞𝐬 」:  ${nu}\n🔧「 𝐓𝐨𝐭𝐚𝐥 𝐀𝐝𝐦𝐢𝐧𝐢𝐬𝐭𝐫𝐚𝐭𝐨𝐫𝐬 」: ${qtv} \n「 𝐈𝐧𝐜𝐥𝐮𝐝𝐞 」:\n${listad}\n🔧「 𝐓𝐨𝐭𝐚𝐥 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐌𝐞𝐬𝐬𝐚𝐠𝐞𝐬 」: ${sl} msgs.\n\n𝐌𝐚𝐝𝐞 𝐖𝐢𝐭𝐡 ❤️ 𝐁𝐲: ᑘᒪᒪᗩSᕼ ッ `,
+            body: `🔧「 إسـم الـجـروب 」:${threadName}\n🔧「 مـعـرف الـجـروب 」: ${id}\n🔧「 الـمـوافـقـة 」: ${pd}\n🔧「 الـإيـمـوجـي 」: ${icon}\n🔧「 مـعـلـومـات 」: مـن ضـمـنـهـم ${threadMem} عضو\n🔧「 عـدد الـذكـور 」: ${nam}\n🔧「 عـدد الـإنـاث 」:  ${nu}\n🔧「 إجـمـالـي الـأدمـنـيـة 」: ${qtv} \n「 مـن ضـمـنـهـم 」:\n${listad}\n🔧「 الـعـدد الـإجـمـالـي للـرسـائـل 」: ${sl} رسالة`,
             attachment: fs.createReadStream(__dirname + '/cache/1.png')
           },
           event.threadID,
